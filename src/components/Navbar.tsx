293331599ee98dbce91a65159595eb38ac907fa6
Somchai2647 <somchai.b@cvc.ac.th>
2024-03-19 00:46:50 +0700

    Update ALURFIA IN SHELTER title and description, add logo to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,24 +10,42 @@ import {
   NavbarMenuItem,
 } from "@nextui-org/navbar";
 import { Button, ButtonGroup } from "@nextui-org/react";
-import Link from 'next/link';
+import Link from "next/link";
+import Image from "next/image";
 import AcmeLogo from "./AcmeLogo";
+import Logo from "@/components/Svgs/Logo";
 type Props = {};
+
+type MenuItem = {
+  title: string;
+  href: string;
+  target?: "_blank" | "_self";
+};
 
 export default function MainNavbar({}: Props) {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-  const menuItems = [
-    "Profile",
-    "Dashboard",
-    "Activity",
-    "Analytics",
-    "System",
-    "Deployments",
-    "My Settings",
-    "Team Settings",
-    "Help & Feedback",
-    "Log Out",
+  const menuItems: MenuItem[] = [
+    {
+      title: "Home",
+      href: "/",
+      target: "_self",
+    },
+    {
+      title: "Fanpage",
+      href: "https://www.facebook.com/AlurfiaInShelter",
+      target: "_blank",
+    },
+    {
+      title: "Services",
+      href: "/services",
+      target: "_self",
+    },
+    {
+      title: "Login",
+      href: "/login",
+      target: "_self",
+    },
   ];
   return (
     <Navbar onMenuOpenChange={setIsMenuOpen} isBordered>
@@ -36,38 +54,44 @@ export default function MainNavbar({}: Props) {
           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
           className="sm:hidden"
         />
-        <NavbarBrand>
-          <AcmeLogo />
-          <p className="font-bold text-inherit">ACME</p>
-        </NavbarBrand>
+        <Link href={"/"}>
+          <NavbarBrand>
+            {/* <AcmeLogo /> */}
+            <Image src={"/favicon.ico"} width={32} height={32} alt="icon" className="mr-4" />
+            <p className="font-bold text-inherit">ALURFIA</p>
+          </NavbarBrand>
+        </Link>
       </NavbarContent>
 
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem>
-          <Link color="foreground" href="#">
-            About
+          <Link color="foreground" href="/" aria-current="page">
+            Home
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link href="#" aria-current="page">
-            Customers
+          <Link
+            href="https://www.facebook.com/AlurfiaInShelter"
+            target="_blank"
+          >
+            Fanpage
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
+          <Link color="foreground" href="/services">
+            Services
           </Link>
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
+        {/* <NavbarItem className="hidden lg:flex">
           <Link href="#">Login</Link>
         </NavbarItem>
         <NavbarItem>
           <Button as={Link} color="primary" href="#" variant="flat">
             Sign Up
           </Button>
-        </NavbarItem>
+        </NavbarItem> */}
       </NavbarContent>
       <NavbarMenu>
         {menuItems.map((item, index) => (
@@ -81,9 +105,10 @@ export default function MainNavbar({}: Props) {
                   : "foreground"
               }
               className="w-full"
-              href="#"
+              href={item.href}
+              target={item.target}
             >
-              {item}
+              {item.title}
             </Link>
           </NavbarMenuItem>
         ))}
