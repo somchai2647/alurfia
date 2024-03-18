2e986320505618e237b3eb5377fab0a734fd8de0
Somchai2647 <somchai.b@cvc.ac.th>
2024-03-19 01:00:55 +0700

    Refactor Banner component and add logo animation

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,32 +1,39 @@
 import React from "react";
-import Link from 'next/link';
+import Link from "next/link";
+import Image from "next/image";
 
 type Props = {};
 
 export default function HeroBanner({}: Props) {
   return (
     <div>
-      <div
+      <section
         className="bg-cover bg-center h-screen flex items-center justify-center "
         style={{
           // blur the background image on the hero banner
 
-          backgroundImage: `url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)`,
+          backgroundImage: `url(/background2.jpg)`,
         }}
       >
-        <div className="text-center text-white">
+        <div className="text-center text-white ">
+          <div className="w-full flex justify-center">
+            {/* tailwind animation loop scale */}
+            <div className="rounded-full animate-pulse">
+              <Image src="/favicon.ico" width={200} height={200} alt="logo" />
+            </div>
+          </div>
           <h1 className="text-5xl font-bold mb-4">ALURFIA IN SHELTER</h1>
           <p className="text-xl mb-8">
-          ร้านบอร์ดเกม คาราโอเกะ และไอศกรีมซอฟท์เสิร์ฟหน้า มฟล.
+            ร้านบอร์ดเกม คาราโอเกะ และไอศกรีมซอฟท์เสิร์ฟหน้า มฟล.
           </p>
           <Link
             href="/checkin"
-            className="inline-block bg-blue-500 text-white px-8 py-3 text-sm font-medium rounded hover:bg-blue-700"
+            className="inline-block bg-blue-500 text-white px-8 py-3 text-2xl font-medium rounded hover:bg-blue-700"
           >
             Check In
           </Link>
         </div>
-      </div>
+      </section>
     </div>
   );
 }
